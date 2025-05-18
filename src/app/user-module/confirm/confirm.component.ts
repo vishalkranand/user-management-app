efd81e94108aff28cdca0dfe32da1e6708b0d915
vishal.anand <vishal.anand@yatra.com>
2025-05-19 05:20:53 +0530

    added confirm dialog component for delete user confirmation

---
+++ src/app/user-module/confirm/confirm.component.ts
@@ -0,0 +1,16 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm',
+  templateUrl: './confirm.component.html',
+  styleUrls: ['./confirm.component.scss'],
+})
+export class ConfirmComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  ngOnInit(): void {}
+}
