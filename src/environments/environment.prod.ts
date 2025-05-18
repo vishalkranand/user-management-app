1f7687a1e17224ba8634e9bb6033aebb00190f51
vishal.anand <vishal.anand@yatra.com>
2025-05-19 05:03:41 +0530

    added apis urls in env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+
+  //Base URL
+  BASE_URL: 'https://localhost:44300/api',
+
+  // Controllers
+  AUTH: '/Auth',
+  USER: '/User',
+
+  // Actions
+  LOGIN: '/login',
+  REGISTER: '/register',
+  GET_ALL_USERS: '/users',
+  UPDATE_USER_DETAILS: '/update-user-details',
+  DELETE_USER: '/delete-user',
+  DELETE_MULTIPLE_USERS: '/delete-multiple',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  //Base URL
+  BASE_URL: 'https://localhost:44300/api',
+
+  // Controllers
+  AUTH: '/Auth',
+  USER: '/User',
+
+  // Actions
+  LOGIN: '/login',
+  REGISTER: '/register',
+  GET_ALL_USERS: '/users',
+  UPDATE_USER_DETAILS: '/update-user-details',
+  DELETE_USER: '/delete-user',
+  DELETE_MULTIPLE_USERS: '/delete-multiple',
 };
 
 /*
