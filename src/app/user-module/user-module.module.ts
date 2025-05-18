d03c568d47ab4137a3ff5de8b1494ab9f1b2fe24
vishal.anand <vishal.anand@yatra.com>
2025-05-19 05:16:08 +0530

    added user-module for user component routes

---
+++ src/app/user-module/user-module.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserModuleRoutingModule } from './user-module-routing.module';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { AppModule } from '../app.module';
+import { HeaderComponent } from './header/header.component';
+import { MaterialModule } from '../shared/material-ui/material-ui.module';
+import { ConfirmComponent } from './confirm/confirm.component';
+import { EditComponent } from './edit/edit.component';
+
+@NgModule({
+  declarations: [UserDetailsComponent, HeaderComponent, ConfirmComponent, EditComponent],
+  imports: [CommonModule, UserModuleRoutingModule, MaterialModule],
+})
+export class UserModuleModule {}
